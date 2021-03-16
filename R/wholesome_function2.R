e2238fd56702c0d5fe1960ce1d25953f5c5002e4
jf15558 <50243608+jf15558@users.noreply.github.com>
2021-03-16 18:45:58 +0000

    added wholesome_function2

---
+++ R/wholesome_function2.R
@@ -0,0 +1,9 @@
+#' wholesome_function2
+#'
+#' Another function that is wholesome
+#' @return Moar wholesomeness
+#' @export
+
+wholesome_function2 <- function() {
+  print("What would Master Oogway do?")
+}
